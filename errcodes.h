a98897c2ae9eca50faca62a2407703b18ed976b2
il8677 <jiajiaisaac@gmail.com>
2022-11-06 15:46:23 +0100

    Added error codes

---
+++ errcodes.h
@@ -0,0 +1,12 @@
+#ifndef ERR_H
+#define ERR_H
+
+#define ERR_SQL -1 // Internal sql error
+#define ERR_NAME_INVALID -2 // Name given by client is invalid
+#define ERR_INVALID_API_MSG -3 // Internal server error
+#define ERR_USERNAME_EXISTS -4 // Username already exists
+#define ERR_INCORRECT_LOGIN -5 // Username/password wrong
+#define ERR_AUTHENTICATION -6 // Message could not be authenticated
+#define ERR_NO_USER -7 //Not logged in
+
+#endif
