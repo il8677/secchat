68c510b67bc0f7fd7b3d056e6f8163cedf87114b
froalith <joeypei@outlook.com>
2022-12-01 15:35:34 +0100

    linked list del

--- src/client/linkedlist.c
+++ src/client/linkedlist.c
@@ -13,8 +13,20 @@ struct Node {
 void list_add(Node* head, char* name, char* data, uint16_t datalen) {
     struct Node *node = (struct Node *) malloc (sizeof(struct Node));
     node->name = name;
-    node->data = data;
+    node->contents = data;
+    node->next = NULL;
     //datalen?
     while(head->next != NULL) head = head->next;
     head->next = node;
+}
+
+void list_del(Node* head, char*name) {
+    while(head->next != null){
+        if(head->next.name == name) {
+            head->next = head->next.next; 
+            break;
+        }
+        head = head->next;
+    }
+
 }
