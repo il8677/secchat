6b384407952f878abb5aec92e63ad27eefd99877
il8677 <jiajiaisaac@gmail.com>
2022-12-03 08:53:33 +0100

    Fixed broken list_find function

--- src/util/linkedlist.c
+++ src/util/linkedlist.c
@@ -72,7 +72,7 @@ Node* list_init() {
 
 Node* list_find(Node* head, const char* key) {
     while(head->next != NULL){
-        if(head->key == key) return head;
+        if(strcmp(head->key, key)==0) return head;
         head = head->next;
     }
     return NULL;
