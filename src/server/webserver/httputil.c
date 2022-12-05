6b90998927de09c5cd6bb93f52926d9d8d2cd004
il8677 <jiajiaisaac@gmail.com>
2022-12-05 03:08:39 +0100

    api_msg to json update

--- src/server/webserver/httputil.c
+++ src/server/webserver/httputil.c
@@ -5,6 +5,7 @@
 #include <openssl/ssl.h>
 
 #include "../../common/api.h"
+#include "../../util/crypto.h"
 #include "../../../vendor/ssl-nonblock.h"
 
 void sendContentHeader(SSL* ssl, int fd, int length){
@@ -29,32 +30,104 @@ void send400(SSL* ssl, int fd){
     ssl_block_write(ssl, fd, err404, sizeof(err404));
 }
 
+/// @brief Converts an api_msg to json format, encodes encrypted fields in b64
+/// @return A dynamically allocated address where the json string is
+// messy function, but I dont know if its possible to do formatting functions in a clean way
+// Big mallocs too, but json uses b64 to encode binary data
 char* api_msg_to_json(struct api_msg* msg){
-    // TODO: find a better way to be sure to allocate enough space for the json string
-    char* json = malloc(50 + 10 + 10 + MAX_MSG_LEN + MAX_USER_LEN + MAX_USER_LEN);
+    char* priv = NULL;    
+    unsigned int jsonLoc = 0; // Keep track of where in the buffer we have written to
+
+    if (msg->encPrivKeyLen){
+        // B64 encode
+        Base64Encode((unsigned char*)msg->encPrivKey, msg->encPrivKeyLen, (char**)&priv);
+    }
+
+    // json overhead space + additional data
+    int allocatedSize = 50 + (msg->encPrivKeyLen? strlen(priv) : 0) + msg->certLen;
+
+    char* json;
     
+    //TODO: Space limiters
+    // Main message
     switch(msg->type){
         case ERR:
-            sprintf(json, "{\"type\": %d, \"errcode\": %d}", msg->type, msg->errcode);
+            json = malloc(allocatedSize);
+            jsonLoc += sprintf(json, "{\"type\": %d, \"errcode\": %d", msg->type, msg->errcode);
             break;
         case STATUS:
-            sprintf(json, "{\"type\": %d, \"status\": \"%s\"}", msg->type, msg->status.statusmsg);
+            allocatedSize += sizeof(msg->status.statusmsg);
+            json = malloc(allocatedSize);
+            jsonLoc += sprintf(json, "{\"type\": %d, \"status\": \"%s\"", msg->type, msg->status.statusmsg);
             break;
-        case PRIV_MSG:
-            //sprintf(json, "{\"type\": %d, \"timestamp\": %ld, \"msg\":\"%s\", \"from\":\"%s\", \"to\":\"%s\"}", 
-            //msg->type, msg->priv_msg.timestamp, msg->priv_msg.msg, msg->priv_msg.from, msg->priv_msg.to);
-            break;
-        case PUB_MSG:
-            sprintf(json, "{\"type\": %d, \"timestamp\": %ld, \"msg\":\"%s\", \"from\":\"%s\"}", 
-            msg->type, msg->pub_msg.timestamp, msg->pub_msg.msg, msg->pub_msg.from);
-            break;
+        case PRIV_MSG:{
+            // This inefficient, because we're copying a lot of buffers instead of just writing a buffer once, but it's good enough
+
+            // b64 encrypted fields
+            char* frommssg; // We dont need tomsg since server->client messages only contain the one field
+            char* signature;
+
+            Base64Encode((unsigned char*)msg->priv_msg.signature, MAX_ENCRYPT_LEN, &signature);
+            Base64Encode((unsigned char*)msg->priv_msg.frommsg, MAX_ENCRYPT_LEN, &frommssg);
+
+            // Subtract from the size needed since we have b64 encoded strings instead of the messages & signature
+            allocatedSize += strlen(frommssg) + strlen(signature) + sizeof(msg->priv_msg) - MAX_ENCRYPT_LEN * 2;
+
+            json = malloc(allocatedSize);
+            jsonLoc += sprintf(json, "{\"type\": %d, \"signature\": \"%s\", \
+            \"timestamp\": %ld, \"msg\": \"%s\", \"from\":\"%s\", \"to\":\"%s\"", 
+            msg->type, signature, msg->priv_msg.timestamp, frommssg, msg->priv_msg.from, msg->priv_msg.to);
+            
+            free(frommssg);
+            free(signature);
+            break;}
+        case PUB_MSG:{
+            char* signature;
+            Base64Encode((unsigned char*)msg->priv_msg.signature, MAX_ENCRYPT_LEN, &signature);
+
+            // Subtrack from the size needed since we have the b64 string instead of the signature
+            allocatedSize += strlen(signature) + sizeof(msg->pub_msg) - MAX_ENCRYPT_LEN;
+            json = malloc(allocatedSize);
+            jsonLoc += sprintf(json, "{\"type\": %d, \"signature\": \"%s\", \"timestamp\": %ld, \"msg\":\"%s\", \"from\":\"%s\"", 
+            msg->type, signature, msg->pub_msg.timestamp, msg->pub_msg.msg, msg->pub_msg.from);
+
+            free(signature);
+            break;}
         case WHO:
-            sprintf(json, "{\"type\": %d, \"who\": \"%s\"}", msg->type, msg->who.users);
+            allocatedSize += sizeof(msg->who);
+            json = malloc(allocatedSize);
+            jsonLoc += sprintf(json, "{\"type\": %d, \"who\": \"%s\"}", msg->type, msg->who.users);
+            break;
+
+        case LOGINACK:
+            json = malloc(allocatedSize);
+            jsonLoc += sprintf(json, "{\"type\": %d}", msg->type);
+            break;
+        case KEY:
+            json = malloc(allocatedSize);
+            jsonLoc += sprintf(json, "{\"type\": %d}", msg->type);
             break;
 
         default:
+            json = malloc(allocatedSize);
             sprintf(json, "{\"type\": %d}", NONE);
-        }
+            goto cleanup;
+    }
+
+    // Attach extra data
+
+    if(msg->encPrivKeyLen){
+        jsonLoc += sprintf(json + jsonLoc, ", \"privkey\": \"%s\"", priv);
+    }
+
+    if(msg->certLen){
+        jsonLoc += sprintf(json + jsonLoc, ", \"cert\": \"%s\"", msg->cert);
+    }
+
+    sprintf(json + jsonLoc, "}");
+
+    cleanup:
+    free(priv);
 
     return json;
 }
