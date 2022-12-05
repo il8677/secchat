7c21d599635f8f8c7894f55bab7ba9ef6e324c82
il8677 <jiajiaisaac@gmail.com>
2022-12-05 18:42:07 +0100

    WWW debugging

--- src/server/protocols/prot_http.c
+++ src/server/protocols/prot_http.c
@@ -43,8 +43,14 @@ int protht_notify(struct worker_state* n){ // HTTP should never notify
 int protht_send(struct worker_state* wstate, struct api_msg* msg){ // HTTP should never send
     return 1;
 }
+#define ROUTE_DEBUG
 
 int protht_recv(struct worker_state* wstate, struct api_msg* msg){
+    #ifdef ROUTE_DEBUG
+        www_route_free(routes);
+        protht_init();
+    #endif
+
     struct api_state* state = &wstate->api;
     msg->type = NONE; // There is no message interpreted, it is just an HTTP request
 
