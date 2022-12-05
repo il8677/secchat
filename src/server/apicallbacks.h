8fb5f7ad151b966362883f17a406a647adc0d865
il8677 <jiajiaisaac@gmail.com>
2022-12-05 22:44:26 +0100

    Documentation

--- src/server/apicallbacks.h
+++ src/server/apicallbacks.h
@@ -11,7 +11,7 @@ typedef int (*api_send_t)(struct worker_state*, struct api_msg*);
 // Recieves an api message from client, returns 1 on success -1 on error. aip_msg.type could be set to NONE meaning no api_msg recieved
 typedef int (*api_recv_t)(struct worker_state*, struct api_msg*);
 
-// Function pointers to protocol api
+// Function pointers to protocol callbacks
 struct api_callbacks{
   api_send_t send;
   api_recv_t recv;
