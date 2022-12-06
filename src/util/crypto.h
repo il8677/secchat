7822b1de4c96dcc9226bc9abc8fed526152fc955
il8677 <jiajiaisaac@gmail.com>
2022-12-06 01:53:34 +0100

    Readability

--- src/util/crypto.h
+++ src/util/crypto.h
@@ -7,12 +7,32 @@
 
 #define TTP_PATH "clientkeys/ca.cert"
 
+/// @brief Encodes data to B64
+/// @param buffer The string
+/// @param length The length of the data
+/// @param b64text The uninitialized pointer that will contain the output
 int Base64Encode(const unsigned char* buffer, size_t length, char** b64text);
 
+/// @brief Hashes using SHA-1
+/// @param data The data to hash
+/// @param len The length of the data
+/// @param output The buffer to output to
 void crypto_hash(const char* data, uint32_t len, unsigned char* output);
 
+/// @brief Hashes using SHA-2 (256)
+/// @param data The data to hash
+/// @param len The length of the data
+/// @param output The buffer to output to
 void crypto_sha2_hash(const char* data, uint32_t len, const char* salt, unsigned char* output);
 
+/// @brief AES encrpyts / dececrypts
+/// @param bytes The bytes to encrypt/decrpyt
+/// @param bytesLen The length of the bytes
+/// @param password The key
+/// @param iv The initialization vector
+/// @param encrypt 1 for encrypt, 0 for decrypt
+/// @param outLen The place to store the output length
+/// @return A dynamically allocated buffer containing the output data
 char* crypto_aes_encrypt(char* bytes, uint16_t bytesLen, const char* password, const char* iv, char encrypt, uint16_t* outLen);
 
 /// @brief Asks the TTP to generate a certificate and private key for user
@@ -30,12 +50,23 @@ RSA* crypto_parse_RSA_priv_string(const char* rsapriv);
 /// @brief RSA encrypts a message using an X509 certificate as key
 void crypto_RSA_pubkey_encrypt(char* dst, X509* key, char* msg, uint16_t msglen);
 
+/// @brief Decrypts a RSA public key encrypted message
 char* crypto_RSA_privkey_decrypt(RSA* key, const char* msg);
 
+/// @brief Produces a signature for a message
+/// @param key Private key
+/// @param msg The message to sign
+/// @param msglen The length of the message
+/// @param output The place to put the signature
 void crypto_RSA_sign(RSA* key, const char* msg, uint16_t msglen, unsigned char* output);
 
+/// @brief Verifies a signature
+/// @param key X509 cert with the signers public key
+/// @return If the signature matches
 int crypto_RSA_verify(X509* key, const char* msg, uint16_t msglen, const char* signature, uint16_t signaturelen);
 
+/// @brief Verifies if a X509 certificate was signed by the CA and if the name matches
+/// @return If the cert was valid
 int crypto_verify_x509(X509* target, const char* name);
 
 #endif
--- src/util/linkedlist.h
+++ src/util/linkedlist.h
@@ -4,30 +4,49 @@
 #include <stdbool.h>
 
 typedef struct Node Node;
+
+// Callback used in the exec functions
 typedef void(*list_cb_t)(Node* node, void* usr);
 
 struct Node {
     Node* next;
 
-    char* key;
-    char contents[];
+    char* key; // The key of the node
+    char contents[]; // The contents of the node
 };
 
+/// @brief Initializes an empty node
+/// @return The head (dummy) node
 Node* list_init();
+
+/// @brief Frees an entire list
+/// @param head The head of the list
 void list_free(Node* head);
 
-/// @brief Add to a linked list
-/// @return 1 if failed
+/// @brief Adds a node to a list
+/// @param head The head of the list
+/// @param key The key of the node
+/// @param data The data to store in the node
+/// @param datalen The length of the data
+/// @param unique If the insertion should check if the key is unique
+/// @return 1 if the key was not unique and the unique flag was set
 int list_add(Node* head, const char* key, void* data, uint16_t datalen, char unique);
+
+/// @brief Deletes a key from a list
 void list_del(Node* head, const char* key);
 
 /// @brief Executes a callback across a list
 /// @param head the head of the list 
 /// @param key the key to execute on
 /// @param cb the callback to use
-/// @param userData arbitrary data to pass into the function
+/// @param userData arbitrary data to pass into the callback
 /// @param doDelete boolean: delete flag after exec
 void list_exec(Node* head, const char* key, list_cb_t cb, void* userData, char doDelete);
+
+/// @brief Executes a callback across the entire list
+/// @param cb The callback to use 
+/// @param userData arbitrary data to pass into the callback
+/// @param doDelete boolean: delete flag after exec
 void list_exec_all(Node* head, list_cb_t cb, void* userData, char doDelete);
 
 Node* list_find(Node* head, const char* key);
